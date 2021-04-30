4b486cf202a0115e099ab74652facf7c4e04e1d2
Eirik Remen <eirikremen@Eiriks-MacBook-Pro.local>
2021-04-30 13:45:27 +0200

    sell and purchase test

--- src/contracts/master_swap.sol
+++ src/contracts/master_swap.sol
@@ -6,6 +6,13 @@ contract master_swap {
  string public name = "master_swap Instant Exchange";
  Token public token;
  uint public rate = 100;
+
+ event TokenPurchased(
+ 	address account,
+ 	address token,
+ 	uint amount,
+ 	uint rate
+ );
  
 	constructor(Token _token) public{
 		token = _token;
@@ -14,7 +21,37 @@ contract master_swap {
  function buyTokens() public payable {
  	// Calculate the number of tokens to buy
  	uint tokenAmount = msg.value * rate;
+ 	
+ 	//Require that masterswap has enough tokens
+ 	require(token.balanceOf(address(this)) >= tokenAmount);
+
+ 	//Transfer tokens to the user
  	token.transfer(msg.sender, tokenAmount); 
+ 
+ 	//Emit an event
+ 	emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
  }
+
+ function sellTokens(uint _amount) public {
+ 	//Calculate the amount of Ether to redeem
+ 	uint etherAmount = _amount / rate;
+
+ //Perform the sale
+ 	token.transferFrom(msg.sender, address(this), _amount);
+ 	msg.sender.transfer(etherAmount);
+ }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
  
--- test/master_swap.test.js
+++ test/master_swap.test.js
@@ -9,7 +9,7 @@ function tokens(n) {
 	return web3.utils.toWei(n,"ether");	
 }
 
-contract('master_swap', ([deployer, investor]) => {
+contract('master_swap', ([deployer, investor ]) => {
 	let tokenContract, masterSwapContract
 
 	before(async () => {
@@ -39,8 +39,55 @@ contract('master_swap', ([deployer, investor]) => {
 	})
 
 	describe("buyTokens()", async () => {
+		let result
+
+		before(async () => {
+			//Purchase tokens before each example
+			result = await masterSwapContract.buyTokens({ from: investor, value: web3.utils.toWei("1","ether")})
+	})
+
 		it("Allows user to instantly purchase tokens from masterSwapContract for a fixed price", async() => {
-			await masterSwapContract.buyTokens({ from: investor, value: web3.utils.toWei("1","ether")})
+			//Check investor recive token balance after purchase
+			let investorBalance = await tokenContract.balanceOf(investor)
+			assert.equal(investorBalance.toString(), tokens("100"))			
+		
+			//Check MasterSwap balance after purchase
+			let MasterSwapBalance
+			MasterSwapBalance = await tokenContract.balanceOf(masterSwapContract.address)
+			assert.equal(MasterSwapBalance.toString(), tokens("999900"))
+			MasterSwapBalance = await web3.eth.getBalance(masterSwapContract.address)
+			assert.equal(MasterSwapBalance.toString(), web3.utils.toWei("1", "ether"))
+		
+			const event = result.logs[0].args
+			assert.equal(event.account, investor)
+			assert.equal(event.token, tokenContract.address)
+			assert.equal(event.amount.toString(),tokens("100").toString())
+			assert.equal(event.rate.toString(), "100")
+		})
+	})
+
+
+	describe("sellTokens()", async () => {
+		let result
+
+		before(async () => {
+			// Investor must approve tokens before the purchase
+			await tokenContract.approve(masterSwapContract.address, tokens("100"), { from: investor })
+			//Investor sell tokens
+			result = await masterSwapContract.sellTokens(tokens("100"), { from: investor })
+		})
+
+		it("Allows user to instantly sell tokens to masterSwapContract for a fixed price", async() => {
+			//Check investor recive token balance after sell
+			let investorBalance = await tokenContract.balanceOf(investor)
+			assert.equal(investorBalance.toString(), tokens("0"))			
+		
+			//Check MasterSwap balance after sell
+			let MasterSwapBalance
+			MasterSwapBalance = await tokenContract.balanceOf(masterSwapContract.address)
+			assert.equal(MasterSwapBalance.toString(), tokens("1000000"))
+			MasterSwapBalance = await web3.eth.getBalance(masterSwapContract.address)
+			assert.equal(MasterSwapBalance.toString(), web3.utils.toWei("0", "ether"))
 		})
 	})
 
